d7e67ea3d9d8255508d47043ff80ef0a5e4311d0
Likitha sri Maddipatla <likithasrimaddipatla@gmail.com>
2026-04-01 22:19:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,15 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-# ---------------------------------------------------
 # Load the data
-# ---------------------------------------------------
+
 movies = pd.read_pickle('movie_list (1).pkl')
 
 with open('similarity (1).pkl', 'rb') as f:
     similarity = pickle.load(f)
 
-# ---------------------------------------------------
 # Recommendation function
-# ---------------------------------------------------
+
 def recommend(movie):
     try:
         index = movies[movies['title'].str.lower() == movie.lower()].index[0]
@@ -22,9 +20,8 @@ def recommend(movie):
     distances = sorted(enumerate(similarity[index]), key=lambda x: x[1], reverse=True)
     return [movies.iloc[i[0]].title for i in distances[1:6]]
 
-# ---------------------------------------------------
 # Streamlit UI
-# ---------------------------------------------------
+
 st.title('Movie Recommendation System')
 
 movie_list = movies['title'].values
